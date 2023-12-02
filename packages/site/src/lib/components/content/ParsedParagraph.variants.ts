1e50de7e1790583d812c733adf789521b37a7163
j-hvsb <jacob@hvsb.app>
2023-12-02 09:31:50 +0800

    fix check errors

--- packages/site/src/lib/components/content/ParsedParagraph.variants.ts
+++ packages/site/src/lib/components/content/ParsedParagraph.variants.ts
@@ -1,7 +1,7 @@
-import type { Variants } from 'kitbook';
+import type { Variant } from 'kitbook';
 import type Component from './ParsedParagraph.svelte';
 
-export const variants: Variants<typeof Component> = [
+export const variants: Variant<Component>[] = [
   {
     name: 'Normal references',
     props: {
